8e1323fed39f0ec026c45c2ff11ee67d85b4f566
manojupadhya11 <manojupadhya11@gmail.com>
2024-09-02 08:54:00 +0530

    updated shell script of simple addition calculator

--- Shell Scripting Programs/03_Simple_Calculator.sh
+++ Shell Scripting Programs/03_Simple_Calculator.sh
@@ -3,3 +3,6 @@ echo "Enter two numbers"
 read num1
 read num2
 echo "Addition: $(($num1 +$num2))"
+echo "Subtratcion: $(($num1 - num2))"
+echo "Multiplication: $(($num1 * $num2))"
+echo "Division:  $(($num1/$num2))"
